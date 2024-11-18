75a47f94a67f002081f805ed52a5bc8de4654fd5
Priyanshu rajak <rajakpriyanshu581@gmail.com>
2024-11-19 04:15:45 +0530

    Integrate Stream

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const zlib = require("zlib");
+const port = 3000;
+const status = require("express-status-monitor");
+
+app.use(status());
+
+fs.createReadStream("./sample.txt")
+  .pipe(zlib.createGzip())
+  .pipe(fs.createWriteStream("./sample.zip"));
+
+app.get("/file", (req, res) => {
+  const stream = fs.createReadStream("Sample.txt", "utf-8");
+  stream.on("data", (chunk) => {
+    res.write(chunk);
+  });
+  stream.on("end", () => {
+    res.end();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
